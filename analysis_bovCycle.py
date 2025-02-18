90a046e70c0e96318515ccfa4d305f01d0b1fbf6
Oto Stanko <493068@fi.muni.cz>
2025-02-18 19:53:13 +0100

    Analysis be cycle added BNs

--- analysis_bovCycle.py
+++ analysis_bovCycle.py
@@ -4,20 +4,25 @@ import os
 from supporting_scripts import find_first_cycle, create_formula_for_path
 
 be_model = os.path.join(os.getcwd(), "model_2_bovine-estrous")
-boolnet_be_model_path = os.path.join(be_model, "BoolNet", "bovine-estrous_maxK4_colored_edges.aeon")
-euler_like_be_model_path = os.path.join(be_model, "euler-like_transformation", "first_model.aeon")
-euler_like_automated_be_model_path = os.path.join(be_model, "euler-like_transformation", "bovine-estrous-cycle_model.aeon")
-sailor_be_model_path = os.path.join(be_model, "SAILoR", "bovine-estrous_model_binarised.aeon")
-sketchBook_be_model_path = os.path.join(be_model, "SketchBook", "sat_networks_1", "candidate_1.aeon")
-be_model_paths = [boolnet_be_model_path, euler_like_automated_be_model_path,
-                  sailor_be_model_path, sketchBook_be_model_path]
+boolnet_be_model_path_full_const = os.path.join(be_model, "BoolNet", "bovine-estrous_full_constraints_colored_edges.aeon")
+boolnet_be_model_path_maxK4 = os.path.join(be_model, "BoolNet", "bovine-estrous_maxK4_colored_edges.aeon")
+euler_like_automated_be_model_path = os.path.join(be_model, "euler-like_transformation", "bovine-estrous-cycle_model_colored_edges.aeon")
+sailor_be_model_path = os.path.join(be_model, "SAILoR", "colored_bovine-estrous_model_binarised.aeon")
+sketchBook_be_model_path = os.path.join(be_model, "SketchBook", "candidate_1.aeon")
+sketchBook_be_model_path_2 = os.path.join(be_model, "SketchBook", "candidate_2.aeon")
+be_model_paths = [boolnet_be_model_path_full_const, boolnet_be_model_path_maxK4,
+                  euler_like_automated_be_model_path,
+                  sailor_be_model_path, sketchBook_be_model_path,
+                  sketchBook_be_model_path_2]
 
 
 cycles, head = find_first_cycle(os.path.join(be_model, "bov_cycle_ODE_sim_columns_binarized_simplified_auto.csv"))
+print([len(cycle) for cycle in cycles])
 cycles_intersect = cycles[0]
 for i in range(1, len(cycles)):
     cycles_intersect = [val for val in cycles_intersect if val in cycles[i]]
 cycles_intersect.append(cycles_intersect[0])
+print(len(cycles_intersect))
 path_formula_ef, basic_transitions_ef = create_formula_for_path(cycles_intersect, head, on_non_triv_att=True)
 
 msg_ok = ">OK"
